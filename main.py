56c94f4cc4e37fde1c8069c3e95d8a9923428b4c
Shane McDonough <shanemcdman@gmail.com>
2020-05-27 01:22:28 -0700

    Create open_file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sys import argv
+from os import system
 
-note_folder_path = "C:\\Users\\Shane\\Dropbox\\Desktop\\Coding\\python\\notes\\note_folder\\"
+NOTE_FOLDER_PATH = "C:\\Users\\Shane\\Dropbox\\Desktop\\Coding\\python\\notes\\note_folder\\"
 
 def parse_argv() -> ("file_name", "folder_name"):
     """ converts argv into a more usable format
@@ -22,12 +23,25 @@ def parse_argv() -> ("file_name", "folder_name"):
         file_name += ".txt"
     return file_name, folder_name
 
+def open_file(file_name, folder_name) -> None:
+    """ Opens the file in gvim
+    :file_name: The name of the file
+    :folder_name: the name of the folder if none do not use folder
+    :returns: None
+    """
+    file_path = NOTE_FOLDER_PATH
+    if folder_name != None:
+        file_path += folder_name + "\\"
+    file_path += file_name
+    system("gvim " + file_path)
+
 def main():
     try:
         file_name, folder_name = parse_argv()
     except Exception as e:
         print("Error: " + str(e))
         return
+    open_file(file_name, folder_name)
 
 if __name__ == "__main__":
     main()
