a103c988fe943044ae77bec2a724730d846eee43
Shane McDonough <shanemcdman@gmail.com>
2020-05-27 02:03:00 -0700

    Add list.py

---
+++ list.py
@@ -0,0 +1,25 @@
+import os
+from sys import argv
+from main import NOTE_FOLDER_PATH
+
+def parse_argv() -> None or "folder_name":
+    _ = argv.pop(0)
+    length = len(argv)
+    if length == 0:
+        return ""
+    elif length > 1:
+        raise(Exception("Too many arguments"))
+    return argv[0]
+
+
+def main():
+    try:
+        folder_name = parse_argv()
+    except Exception as e:
+        print("Error: " + str(e))
+        return
+    os.system('ls ' + NOTE_FOLDER_PATH + "\\" + folder_name)
+
+
+if __name__ == "__main__":
+    main()
