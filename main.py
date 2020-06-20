40dcbdc5292f5b9902198e049661cad89aaf9e04
Shane McDonough <shanemcdman@gmail.com>
2020-06-19 21:51:30 -0700

    No longer support folder as parameter, allow flags

--- main.py
+++ main.py
@@ -4,65 +4,68 @@ from sys import argv
 
 NOTE_FOLDER_PATH = "C:\\Users\\Shane\\Dropbox\\Desktop\\Coding\\python\\notes\\note_folder\\"
 
-def parse_argv() -> ("file_name", "folder_name"):
+def parse_argv() -> ("relative_path", "gvim_flags"):
     """ converts argv into a more usable format
-    :returns: a tuple of the file name and the folder name
-    folder_name may be None
+    :returns: a tuple of the relative path name and the flags for gvim
     """
     _ = argv.pop(0)
+    gvim_flags = []
+    for arg in argv:
+        if arg.startswith('-'):
+            gvim_flags.append(arg)
+    for flag in gvim_flags:
+        argv.remove(flag)
+    gvim_flags = " ".join(gvim_flags)
     length = len(argv)
-    if length > 2:
+    if length > 1:
         raise(Exception("Too many arguments"))
     elif length < 1:
         raise(Exception("Too few arguments"))
     elif length == 1:
-        file_name = argv[0]
-        folder_name = None
-    elif length == 2:
-        file_name, folder_name = argv
-    if '.' not in file_name:
-        file_name += ".txt"
-    if "\\" in file_name or "/" in file_name:
-        lst = re.split(r" |\\|/", file_name)
-        file_name = lst.pop(-1)
-        if folder_name != None:
-            folder_name += "\\" + "\\".join(lst)
-        else:
-            folder_name = "\\".join(lst)
-    return file_name, folder_name
+        relative_path = argv[0]
+    if '.' not in relative_path:
+        relative_path += ".txt"
+    return relative_path, gvim_flags
 
-def open_file(file_name, folder_name) -> None:
+def open_file(relative_path: str, gvim_flags: str) -> None:
     """ Opens the file in gvim
-    :file_name: The name of the file
-    :folder_name: the name of the folder if none do not use folder
+    :relative_path: 
+    :gvim_flags: flags to pass to gvim directly
     :returns: None
     """
+    if "\\" in relative_path or "/" in relative_path:
+        lst = re.split(r" |\\|/", relative_path)
+        file_name = lst.pop(-1)
+        folder_name = "\\".join(lst) + "\\"
+    else:
+        folder_name = ""
+        file_name = relative_path
     file_path = NOTE_FOLDER_PATH
-    if folder_name != None:
-        file_path += folder_name + "\\"
+    if folder_name != "":
+        file_path += folder_name
         if os.path.exists(file_path):
             if not os.path.isdir(file_path):
                 raise(Exception("Folder name already in use by another file"))
         else:
             os.makedirs(file_path)
     file_path += file_name
-    os.system("gvim " + file_path)
+    os.system("gvim " + file_path + " " + gvim_flags)
 
 def print_usage() -> None:
     """Print the correct usage of the command"""
     print(
-            "Usage of command:\n"+
-            "\tnote {file path} {folder(optional)}"
+            "Usage of command:\n" +
+            "\tnote {relative path} {gvim flag(optional)}"
         )
 
 def main():
     try:
-        file_name, folder_name = parse_argv()
+        relative_path, gvim_flags = parse_argv()
     except Exception as e:
         print("Error: " + str(e))
         print_usage()
         return
-    open_file(file_name, folder_name)
+    open_file(relative_path, gvim_flags)
 
 if __name__ == "__main__":
     main()
