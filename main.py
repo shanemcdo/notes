646b961f56f35b92e808aa2832fafd0d4da47c23
Shane McDonough <shanemcdman@gmail.com>
2021-12-18 18:14:10 -0800

    Use os.path to convert to work on all systems

--- main.py
+++ main.py
@@ -3,38 +3,39 @@ import re
 import subprocess
 from sys import argv
 
-NOTE_FOLDER_PATH = "C:\\Users\\Shane\\Dropbox\\note_folder\\"
+NOTE_FOLDER_PATH = os.path.normpath(os.environ['NOTE_FOLDER'])
+EDITOR = os.environ['EDITOR']
 
-def parse_argv() -> ("relative_path", "gvim_flags", int):
+def parse_argv() -> ("relative_path", "vim_flags", int):
     """ converts argv into a more usable format
     :returns: a tuple of the relative path name and the flags for gvim and the number of rotations to encode
     """
     _ = argv.pop(0)
     relative_paths = []
-    gvim_flags = ""
+    vim_flags = ""
     next_arg_is_n = False
     n = 0
     for arg in argv:
         if arg == '--rotn':
             next_arg_is_n = True
         elif arg.startswith('-'):
-            gvim_flags += arg + " "
+            vim_flags += arg + " "
         elif next_arg_is_n:
             n = int(arg)
             next_arg_is_n = False
         else:
             if '.' not in arg:
                 arg += ".txt"
-            relative_paths.append(arg)
+            relative_paths.append(os.path.normpath(arg))
     length = len(relative_paths)
     if length < 1:
         raise(Exception("Too few arguments"))
-    return relative_paths, gvim_flags, n
+    return relative_paths, vim_flags, n
 
-def open_file(relative_paths: list, gvim_flags: str, n: int) -> None:
+def open_file(relative_paths: list, vim_flags: str, n: int) -> None:
     """ Opens the file in gvim
     :relative_path: a list of relative paths
-    :gvim_flags: flags to pass to gvim directly
+    :vim_flags: flags to pass to gvim directly
     :n: spaces to rotate in ROTN
     :returns: None
     """
@@ -43,7 +44,7 @@ def open_file(relative_paths: list, gvim_flags: str, n: int) -> None:
         if "\\" in relative_path or "/" in relative_path:
             lst = re.split(r" |\\|/", relative_path)
             file_name = lst.pop(-1)
-            folder_name = "\\".join(lst) + "\\"
+            folder_name = os.path.normpath("/".join(lst) + "/")
         else:
             folder_name = ""
             file_name = relative_path
@@ -51,19 +52,19 @@ def open_file(relative_paths: list, gvim_flags: str, n: int) -> None:
         if '.' not in file_name:
             file_name += ".txt"
         if folder_name != "":
-            file_path += folder_name
+            file_path = os.path.join(file_path, folder_name)
             if os.path.exists(file_path):
                 if not os.path.isdir(file_path):
                     raise(Exception("Folder name already in use by another file"))
             else:
                 os.makedirs(file_path)
-        file_path += file_name
+        file_path = os.path.join(file_path, file_name)
         file_paths.append(file_path)
     if n != 0:
-        os.system("ROTNe " + str(n) + " "+ ' '.join(file_paths) + " " + gvim_flags)
+        # os.system("ROTNe " + str(n) + " "+ ' '.join(file_paths) + " " + vim_flags)
+        pass
     else:
-        os.system("start gvim " + ' '.join(file_paths) + " " + gvim_flags)
-
+        os.system(f'{EDITOR} {" ". join(file_paths)} {vim_flags}')
 
 def print_usage() -> None:
     """Print the correct usage of the command"""
@@ -77,12 +78,12 @@ def print_usage() -> None:
 
 def main():
     try:
-        relative_path, gvim_flags, n = parse_argv()
+        relative_path, vim_flags, n = parse_argv()
     except Exception as e:
         print("Error: " + str(e))
         print_usage()
         return
-    open_file(relative_path, gvim_flags, n)
+    open_file(relative_path, vim_flags, n)
 
 if __name__ == "__main__":
     main()
