d6a748038875ca2a0a891ba335ec8baaa038fd92
Shane McDonough <shanemcdman@gmail.com>
2022-05-25 19:51:10 -0700

    Pass flags into ls

--- list.py
+++ list.py
@@ -4,33 +4,34 @@ import os
 from sys import argv
 from main import NOTE_FOLDER_PATH
 
-def parse_argv() -> None or "folder_name":
-    """ Parse argv into something more usable
-    :returns: name of the folder to be searched or an empty string
-    """
-    _ = argv.pop(0)
-    length = len(argv)
-    if length == 0:
-        return ""
-    elif length > 1:
-        raise(Exception("Too many arguments"))
-    return os.path.normpath(argv[0])
-
 def print_usage() -> None:
     """Print the correct usage of the command"""
     print(
-            "Usage of command:\n"+
-            "\tnls {file path(optional)}"
+            "Usage of command:\n"
+            "\tnls A{file path(optional)} [flags]\n"
+            "\t\tflags: flags to be passed into the ls program"
         )
 
 def main():
     try:
-        folder_name = parse_argv()
+        _ = argv.pop(0)
+        folder_name = None
+        flags = []
+        for arg in argv:
+            if arg.startswith('-'):
+                flags.append(arg)
+            elif folder_name is None:
+                folder_name = arg
+            else:
+                raise RuntimeError('Too many arguments')
     except Exception as e:
         print("Error: " + str(e))
         print_usage()
         return
-    os.system('ls ' + os.path.join(NOTE_FOLDER_PATH, folder_name))
+    path = NOTE_FOLDER_PATH
+    if folder_name:
+        path = os.path.join(path, folder_name)
+    os.system(f'ls {" ".join(flags)} ' + path)
 
 
 if __name__ == "__main__":
